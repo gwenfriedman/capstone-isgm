ad0026e0f391ffb90a6de1b42dcc783c05e5b65e
Gwendolyn Friedman <gwenrfriedman@gmail.com>
2022-03-01 15:12:35 -0500

    remove image squishing

--- src/ImageTransition2.js
+++ src/ImageTransition2.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import dutchRoom from './images/evidence-bg.png'
-import courtyard from './image/courtyard.png'
+import courtyard from './images/courtyard.png'
 import {Link} from "react-router-dom";
 
 function ImageTransition(){
     return(
         <div>
             <Link to="/transition" style={{position: "absolute", backgroundColor: "white"}}> back </Link>
-            <img style={{height: "100vh", width: "100vw"}} src={courtyard} alt="dutch room"/>
+            <img style={{width: "100vw"}} src={courtyard} alt="dutch room"/>
         </div>
     )
 }
